a9635ffdd8d69d848137fee1c811b4ac70fbd3c7
octahedrone <octahedrone@gmail.com>
2015-07-30 00:00:12 +0300

    Fixing karma tests

--- app/components/version/interpolate-filter_test.js
+++ app/components/version/interpolate-filter_test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('AMS.version module', function() {
-  beforeEach(module('myApp.version'));
+  beforeEach(module('AMS.version'));
 
   describe('interpolate filter', function() {
     beforeEach(module(function($provide) {
