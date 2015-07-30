e0948e6f80e917e209926d51199100354aafe078
octahedrone <octahedrone@gmail.com>
2015-07-31 01:10:00 +0300

    Switching to phantomjs test runner as a faster alternative to chrome

--- karma.conf.js
+++ karma.conf.js
@@ -15,11 +15,12 @@ module.exports = function(config){
 
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+    browsers : ['PhantomJS'],
 
     plugins : [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
+            //'karma-chrome-launcher',
+            //'karma-firefox-launcher',
+            'karma-PhantomJS-launcher',
             'karma-jasmine',
             'karma-junit-reporter'
             ],
