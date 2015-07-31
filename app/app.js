6fc4577c1d4066f3608a7679c6052962626fac89
octahedrone <octahedrone@gmail.com>
2015-07-31 13:52:44 +0300

    Adding top menu and navbar to the main application view. Empty donor view was removed.

--- app/app.js
+++ app/app.js
@@ -2,10 +2,30 @@
 
 // Declare app level module which depends on views, and components
 angular.module('AMS', [
-  'ngRoute',
-  'AMS.donor',
+  'eehNavigation',
+  'pascalprecht.translate',
+  'ui.bootstrap',
+  'ui.router',
   'AMS.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/donor'});
+])
+.config(['eehNavigationProvider', function (eehNavigationProvider) {
+    // Add nested user links to the "foo" menu.
+    eehNavigationProvider
+    .menuItem('foo.user', {
+        text: 'Me',
+        iconClass: 'fa-user'
+    })
+    .menuItem('foo.user.profile', {
+        text: 'User Profile',
+        iconClass: 'fa-user',
+        href: '/user-profile'
+    });
+
+    // Add a menu item that links to "/home" to the "bar" menu.
+    eehNavigationProvider
+    .menuItem('bar.home', {
+        text: 'Home',
+        iconClass: 'fa-home',
+        href: '/home'
+    });
 }]);
