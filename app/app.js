4bbeec7939a9eda3dcfd24fd86a33c48367508e3
octahedrone <octahedrone@gmail.com>
2015-08-10 23:42:48 +0300

    Adding initial state transition

--- app/app.js
+++ app/app.js
@@ -2,8 +2,12 @@
 
 // Declare app level module which depends on views, and components
 angular.module('AMS', [
+  'ui.router',
   // -- app modules
   'AMS.version',
   'AMS.states',
   'AMS.navigation',
 ])
+.run(['$state', function($state){
+  $state.go('home');
+}]);
